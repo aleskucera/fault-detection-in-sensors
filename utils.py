f61ff5595fb403e096336d9113cd932606599ce8
Aleš Kučera <kuceral4@fel.cvut.cz>
2024-01-07 11:40:01 +0100

    Change

--- utils.py
+++ utils.py
@@ -69,14 +69,12 @@ def filter_outliers(df: pd.DataFrame, z_thresh: float) -> pd.DataFrame:
 
     return df
 
-
-def inject_outliers(df: pd.DataFrame, column: str, prob: float, outlier_threshold: float, limits: tuple) -> pd.DataFrame:
+def inject_outliers(data: np.ndarray, prob: float, outlier_threshold: float, limits: tuple) -> np.ndarray:
     """Simulate sensor faults by injecting outliers into a column. The probability
     of an outlier being injected is given by `prob`.
 
     Args:
-        df (pd.DataFrame): The dataframe to inject outliers into.
-        column (str): The column to inject outliers into.
+        df (np.ndarray): The dataframe to inject outliers into.
         prob (float): The probability of an outlier being injected.
         outlier_threshold (float): The threshold for determining outliers.
         limits (tuple): The lower and upper limits of the column.
@@ -87,84 +85,142 @@ def inject_outliers(df: pd.DataFrame, column: str, prob: float, outlier_threshol
     """
 
     # Create a copy of the dataframe
-    df = df.copy()
+    data = data.copy()
 
     # Compute the mean and standard deviation of the column
-    data = df[column]
     mean, std = data.mean(), data.std()
 
     # Compute the lower and upper bounds for outliers
     lower, upper = mean - outlier_threshold * std, mean + outlier_threshold * std
 
     # Generate a random outlier value for each row (upper outlier or lower outlier)
-    for i in range(1, len(df)-1):
+    for i in range(1, len(data)-1):
         if np.random.random() < prob:
+            print(f"Outlier injected at row {i}")
             if np.random.random() < 0.5:
-                df.loc[i, column] = np.random.uniform(limits[0], lower)
+                data[i] = np.random.uniform(limits[0], lower)
             else:
-                df.loc[i, column] = np.random.uniform(upper, limits[1])
+                data[i] = np.random.uniform(upper, limits[1])
 
-    return df
+    return data
 
-
-def inject_offset(df: pd.DataFrame, column: str, offset: float, failure_rate: float) -> pd.DataFrame:
+def inject_offset(data: np.ndarray, offset: float, failure_rate: float) -> np.ndarray:
     """Inject an offset into a column. The offset is introduced at a random row
     based on the exponential distribution with the given failure rate. After the offset
     is introduced, it is added to each subsequent row.
 
     Args:
-        df (pd.DataFrame): The dataframe to inject an offset into.
-        column (str): The column to inject an offset into.
+        df (np.ndarray): The dataframe to inject an offset into.
         offset (float): The offset to inject.
         failure_rate (float): The average number of rows between each offset injection.
 
     Returns:
-        pd.DataFrame: The dataframe with the offset injected.
+        np.ndarray: The dataframe with the offset injected.
 
     """
 
     # Create a copy of the dataframe
-    df = df.copy()
+    data = data.copy()
 
     # Compute the row, where the offset is introduced
     row = np.random.exponential(failure_rate)
 
     # Inject the offset into the dataframe
-    df.loc[row:, column] += offset
+    data[int(row):] += offset
 
-    return df
+    return data
 
-
-def inject_drift(df: pd.DataFrame, column: str, drift_slope: float, failure_rate: float) -> pd.DataFrame:
+def inject_drift(data: np.ndarray, drift_slope: float, failure_rate: float) -> np.ndarray:
     """Inject a drift into a column. The drift is introduced at a random row
     based on the exponential distribution with the given failure rate. After the drift
     is introduced, it is added to each subsequent row.
 
     Args:
-        df (pd.DataFrame): The dataframe to inject a drift into.
-        column (str): The column to inject a drift into.
+        df (np.ndarray): The dataframe to inject a drift into.
         drift_slope (float): The slope of the drift.
         failure_rate (float): The average number of rows between each drift injection.
 
     Returns:
-        pd.DataFrame: The dataframe with the drift injected.
+        np.ndarray: The dataframe with the drift injected.
 
     """
 
     # Create a copy of the dataframe
-    df = df.copy()
+    data = data.copy()
 
     # Compute the row, where the drift is introduced
     row = int(np.random.exponential(failure_rate))
 
     # Compute the linear function of the drift error for each subsequent row
-    x = np.arange(len(df[column]) - row) + 1
+    x = np.arange(len(data) - row) + 1
     drift = x * drift_slope
 
     # Inject the drift into the dataframe
-    df.loc[row:, column] += drift
+    data[int(row):] += drift
 
-    return df
+    return data
+
+def detect_anomalies(measurements: np.ndarray, predictions: np.ndarray, error_threshold: float, outlier_threshold: float) -> np.ndarray:
+    """Detect drift in a time series based on the prediction error. The drift is
+    detected by computing the integral of the prediction error and comparing it to the quadratic
+    function of the number of measurements.
+
+    Args:
+        measurements (np.ndarray): The measurements of the time series.
+        predictions (np.ndarray): The predictions of the time series.
+        error_threshold (float): The threshold for determining drift.
+        outlier_threshold (float): The threshold for determining outliers.
+
+    Returns:
+        np.ndarray: The index of the drift.
+    """
+
+    # Compute the prediction error
+    error = measurements - predictions
+    abs_error = abs(error)
+    time = np.arange(len(error))
+
+    # Detect the outliers
+    outlier_indices = np.where(zscore(abs_error) > outlier_threshold)[0]
+    outliers = error[outlier_indices]
+    not_outliers = np.where(zscore(abs_error) <= outlier_threshold)[0]
+
+    plt.plot(time, error, label="Error")
+
+    # Filter the outliers
+    error = error[not_outliers]
+    abs_error = abs_error[not_outliers]
+    time = time[not_outliers]
+
+    plt.scatter(outlier_indices, outliers, label="Outliers", color="red")
+    plt.show()
+
+    plt.plot(time, error, label="Error")
+    plt.show()
+
+    # Keep only the error values that are above the threshold
+    # valid_indices = np.where(abs_error > error_threshold)[0]
+    # error = error[valid_indices]
+    # time = time[valid_indices]
+
+    plt.plot(time, error, label="Error")
+    plt.show()
+
+    # Compute the integral of the prediction error
+    integral = np.cumsum(error)
+
+    # Interpolate the quadratic function of the number of measurements
+    fit, residuals, rank, singular_values, rcond = np.polyfit(time, integral, 1, full=True)
+    model = np.poly1d(fit)
+    linear = model(time)
+
+    # Plot the integral of the prediction error and the quadratic function of the number of measurements
+    plt.plot(time, integral)
+    plt.plot(time, linear)
+    plt.show()
+    print(f"Model: {model}")
+    print(f"Outliers: {outliers}")
+
 
 
 if __name__ == "__main__":
@@ -185,29 +241,37 @@ if __name__ == "__main__":
     plt.ylabel('Temperature')
     plt.show()
 
-    # Inject outliers into the column "T_1ST_AVG"
-    data_outliers = inject_outliers(data, 'T_1ST_AVG', 0.01, 3, (0, 100))
+    data_old = data.copy()
 
-    # Visualize the column "T_1ST_AVG" after injecting outliers
-    data_outliers['T_1ST_AVG'].plot()
-    plt.xlabel('Time')
-    plt.ylabel('Temperature')
-    plt.show()
+    # # Inject outliers into the column "T_1ST_AVG"
+    # data['T_1ST_AVG'] = inject_outliers(data['T_1ST_AVG'], 0.001, 3, (0, 100))
+    #
+    # # Visualize the column "T_1ST_AVG" after injecting outliers
+    # data['T_1ST_AVG'].plot()
+    # plt.xlabel('Time')
+    # plt.ylabel('Temperature')
+    # plt.show()
+    #
+    # print(detect_outliers(data['T_1ST_AVG'], data_old['T_1ST_AVG'], 2))
 
-    # Inject an offset into the column "T_1ST_AVG"
-    data_offset = inject_offset(data, 'T_1ST_AVG', 10, 1000)
-
-    # Visualize the column "T_1ST_AVG" after injecting an offset
-    data_offset['T_1ST_AVG'].plot()
-    plt.xlabel('Time')
-    plt.ylabel('Temperature')
-    plt.show()
+    # # Inject an offset into the column "T_1ST_AVG"
+    # data['T_1ST_AVG'] = inject_offset(data['T_1ST_AVG'], 10, 1000)
+    #
+    # # Visualize the column "T_1ST_AVG" after injecting an offset
+    # data['T_1ST_AVG'].plot()
+    # plt.xlabel('Time')
+    # plt.ylabel('Temperature')
+    # plt.show()
+    #
+    # detect_offset(data['T_1ST_AVG'].array, data_old['T_1ST_AVG'].array, 0.1)
 
     # Inject a drift into the column "T_1ST_AVG"
-    data_drift = inject_drift(data, 'T_1ST_AVG', 0.1, 1000)
+    data['T_1ST_AVG'] = inject_drift(data['T_1ST_AVG'], 0.1, 1000)
 
     # Visualize the column "T_1ST_AVG" after injecting a drift
-    data_drift['T_1ST_AVG'].plot()
+    data['T_1ST_AVG'].plot()
     plt.xlabel('Time')
     plt.ylabel('Temperature')
     plt.show()
+
+    # detect_drift(data['T_1ST_AVG'].array, data_old['T_1ST_AVG'].array, 0.1)
